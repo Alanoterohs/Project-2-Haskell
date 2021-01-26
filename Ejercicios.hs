9c2eff7339855cac47f49c01662328fdcd910ea4
Alanoterohs <alanoterohs@gmail.com>
2021-01-26 12:23:06 -0300

    update exercises

--- Ejercicios.hs
+++ Ejercicios.hs
@@ -30,3 +30,19 @@ data Persona = Decano | Docente Cargo | NoDocente Area | Estudiante Carrera Ingr
 --b) el tipo del constructor Docente es Docente :: Cargo -> Persona
 
 --c)
+data Carrera = Matematica | Fisica | Computacion | Astronomia deriving (Eq)
+type Ingreso = Int
+data Cargo = Titular | Asociado | Adjunto | Asistente | Auxiliar deriving (Eq)
+data Area = Administrativa | Ensenanza | Economica | Postgrado deriving (Eq)
+data Persona = Decano | Docente Cargo | NoDocente Area | Estudiante Carrera Ingreso deriving (Eq)
+
+cuantos_doc :: [Persona] -> Cargo -> Int
+cuantos_doc [] _ = 0 
+cuantos_doc (x:xs) cargo | (x == Docente cargo) = 1 + cuantos_doc xs cargo
+                         | otherwise = cuantos_doc xs cargo
+
+
+--Ejercicio 4)
+primerElemento::[a] -> Maybe a
+primerElemento [] = Nothing
+primerElemento xs = Just (head xs)
